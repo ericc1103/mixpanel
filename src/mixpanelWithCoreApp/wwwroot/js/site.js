20f9db820c3561a4893b8ec65a98c8353f8cae8b
Eric Chong <echong@microsoft.com>
2017-02-10 18:08:26 -0800

    added more telemetires

--- src/mixpanelWithCoreApp/wwwroot/js/site.js
+++ src/mixpanelWithCoreApp/wwwroot/js/site.js
@@ -1,3 +1,8 @@
 ﻿function sendTrack() {    
     mixpanel.track($("#myName").val(), $("#myObject").val());
+
+    mixpanel.track(
+        "site.js",
+        { "desc": "sent by site.js file" }
+    );    
 }
